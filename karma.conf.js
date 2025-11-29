8bc0f86c50744b6aef1bf1541a1ecfd30f45df64
Montse <montse4oral@gmail.com>
2025-11-29 18:32:18 +0100

    Afegit configuració Sonar i tests unitaris

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,38 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// Karma configuration file
+// Updated for SonarQube and karma-coverage
 
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),  // <--- AQUEST és el que hem instal·lat
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+    // Configuració del report de cobertura per a SonarQube
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' }, // <--- Aquest genera el lcov.info vital per al Sonar
+        { type: 'text-summary' }
+      ]
     },
-
-    reporters: ['progress', 'kjhtml'],
+    // Aquí afegim 'coverage' perquè no et doni l'error de "reporter not registered"
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
